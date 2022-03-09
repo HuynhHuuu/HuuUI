8060f2b3c5e287643fb1eeda7792dc08911d5bf4
HuynhHuuu <HUU.H9155@sinhvienhoasen.edu.vn>
2022-03-10 01:55:46 +0700

    HuuUI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'fir-d2c75',
+    projectId: 'test-angular-dc467',
     appId: '1:38043145159:web:b8987ae1b1a00cc21affe2',
-    storageBucket: 'fir-d2c75.appspot.com',
+    storageBucket: 'test-angular-dc467.appspot.com',
     apiKey: 'AIzaSyBYhqSkGqRoiDIJHgo0oFaHBr3G5VVK_A4',
-    authDomain: 'fir-d2c75.firebaseapp.com',
+    authDomain: 'test-angular-dc467.firebaseapp.com',
     messagingSenderId: '38043145159',
   },
   production: true
